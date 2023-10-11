ff8801451b1258450dd47ca16bb7a497d5080bf8
J. Vitor F <joaoaovitor.jobs2001@gmail.com>
2023-10-11 13:38:19 -0300

    feat: add initial setup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Raleway } from 'next/font/google'
+import Head from 'next/head'
+
+const raleway = Raleway({ subsets: ['latin'] })
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className={raleway.className}>
+      <Head>
+        <title>Mesquita & Dallabrida - Advocacia Internacional</title>
+      </Head>
+      <Component {...pageProps} />
+    </div>
+  )
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,8 +2,11 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="pt-BR">
+      <Head>
+        <link rel="icon" href="#" />
+        <meta name="description" content="Mesquita & Dallabrida - Advocacia Internacional" />
+      </Head>
       <body>
         <Main />
         <NextScript />
