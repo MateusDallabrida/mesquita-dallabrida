2447f65c6bcc31a23943c71b54e30864aaa1566f
J. Vitor F <joaoaovitor.jobs2001@gmail.com>
2023-10-14 10:58:17 -0300

    feat: add our services component completely with responsiveness

--- src/components/OurServices.tsx
+++ src/components/OurServices.tsx
@@ -20,7 +20,7 @@ export function OurServices() {
               sizes="100vw"
               className="w-full h-[400px] object-cover rounded-xl"
             />
-            <p className="text-white text-xl font-bold uppercase absolute bottom-10 left-[calc(50%-123.7px)]">
+            <p className="w-full text-center text-white text-xl font-bold uppercase absolute bottom-10">
               Cidadania Portuguesa
             </p>
           </div>
@@ -35,14 +35,134 @@ export function OurServices() {
               sizes="100vw"
               className="w-full h-[400px] object-top object-cover rounded-xl"
             />
-            <p className="text-white text-xl font-bold uppercase absolute bottom-10 left-[calc(50%-123.7px)]">
+            <p className="w-full text-center text-white text-xl font-bold uppercase absolute bottom-10">
               Cidadania Italiana
             </p>
           </div>
         </div>
 
         {/* vertical Services */}
-        <div></div>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-12 mt-12 px-4 xl:px-0">
+          <div className="relative">
+            <div className="w-full h-[400px] bg-main opacity-50 absolute rounded-xl" />
+            <Image
+              src="/service_03.jpg"
+              alt="Visto de Portugal"
+              width={0}
+              height={0}
+              sizes="100vw"
+              className="w-full h-[400px] object-cover rounded-xl"
+            />
+            <p className="w-full text-center px-4 text-white text-xl font-bold uppercase absolute bottom-10">
+              Vistos de Residência
+            </p>
+          </div>
+
+          <div className="relative">
+            <div className="w-full h-[400px] bg-main opacity-50 absolute rounded-xl" />
+            <Image
+              src="/service_04.jpg"
+              alt="Martelo de Juíz"
+              width={0}
+              height={0}
+              sizes="100vw"
+              className="w-full h-[400px] object-cover rounded-xl"
+            />
+            <p className="w-full text-center px-4 text-white text-xl font-bold uppercase absolute bottom-4">
+              Inscrição de advogado na OAP
+            </p>
+          </div>
+
+          <div className="relative">
+            <div className="w-full h-[400px] bg-main opacity-50 absolute rounded-xl" />
+            <Image
+              src="/service_05.jpg"
+              alt="Cidade Portuguesa"
+              width={0}
+              height={0}
+              sizes="100vw"
+              className="w-full h-[400px] object-cover rounded-xl"
+            />
+            <p className="w-full text-center px-4 text-white text-xl font-bold uppercase absolute bottom-4">
+              Autorizações de Residência
+            </p>
+          </div>
+
+          <div className="relative">
+            <div className="w-full h-[400px] bg-main opacity-50 absolute rounded-xl" />
+            <Image
+              src="/service_06.jpg"
+              alt="Um casal segurando um buquê de flores"
+              width={0}
+              height={0}
+              sizes="100vw"
+              className="w-full h-[400px] object-cover rounded-xl"
+            />
+            <p className="w-full text-center px-4 text-white text-xl font-bold uppercase absolute bottom-10">
+              Casamento
+            </p>
+          </div>
+
+          <div className="relative">
+            <div className="w-full h-[400px] bg-main opacity-50 absolute rounded-xl" />
+            <Image
+              src="/service_07.jpg"
+              alt="Pessoas analizando relatórios numa mesa"
+              width={0}
+              height={0}
+              sizes="100vw"
+              className="w-full h-[400px] object-cover rounded-xl"
+            />
+            <p className="w-full text-center px-4 text-white text-xl font-bold uppercase absolute bottom-4">
+              Abertura de atividade e empresas
+            </p>
+          </div>
+
+          <div className="relative">
+            <div className="w-full h-[400px] bg-main opacity-50 absolute rounded-xl" />
+            <Image
+              src="/service_08.jpg"
+              alt="Um pessoa carimbando um documento"
+              width={0}
+              height={0}
+              sizes="100vw"
+              className="w-full h-[400px] object-cover rounded-xl"
+            />
+            <p className="w-full text-center px-4 text-white text-xl font-bold uppercase absolute bottom-10">
+              Atos notariais
+            </p>
+          </div>
+
+          <div className="relative">
+            <div className="w-full h-[400px] bg-main opacity-50 absolute rounded-xl" />
+            <Image
+              src="/service_09.jpg"
+              alt="Um garota segurando um diploma"
+              width={0}
+              height={0}
+              sizes="100vw"
+              className="w-full h-[400px] object-cover rounded-xl"
+            />
+            <p className="w-full text-center px-4 text-white text-xl font-bold uppercase absolute bottom-4">
+              Reconhecimento de diplomas
+            </p>
+          </div>
+
+          <div className="relative">
+            <div className="w-full h-[400px] bg-main opacity-50 absolute rounded-xl" />
+            <Image
+              src="/service_10.png"
+              alt="Duas pessoas segunrando as mãos"
+              width={0}
+              height={0}
+              sizes="100vw"
+              className="w-full h-[400px] object-cover rounded-xl"
+            />
+            <p className="w-full text-center px-4 text-white text-xl font-bold uppercase absolute bottom-4">
+              Reconhecimento de União Estável
+            </p>
+          </div>
+        </div>
       </div>
     </section>
   )
