81bc532d50a0595b07130f06b52bb1662cbb4840
J. Vitor F <joaoaovitor.jobs2001@gmail.com>
2023-10-16 18:45:26 -0300

    feat: add time and cotation api

--- src/components/Communication.tsx
+++ src/components/Communication.tsx
@@ -1,6 +1,31 @@
+import { useState, useEffect } from 'react'
 import { BsFacebook, BsInstagram, BsLinkedin, BsWhatsapp, BsYoutube } from 'react-icons/bs'
 
 export function Communication() {
+  const fusoLisboa = "Europe/Lisbon"
+  const agoraEmLisboa = new Date().toLocaleString('pt-BR', {
+    timeZone: fusoLisboa,
+    hour12: false,
+    hour: 'numeric',
+    minute: 'numeric'
+  })
+  const [cotacaoEuro, setCotacaoEuro] = useState(0)
+
+  useEffect(() => {
+    const obterCotacaoEuro = async () => {
+      try {
+        const response = await fetch('https://economia.awesomeapi.com.br/last/EUR-BRL')
+        const data = await response.json()
+
+        setCotacaoEuro(Number(data.EURBRL.ask))
+      } catch (error: any) {
+        console.error('Erro ao obter a cotação do Euro:', error.message)
+      }
+    }
+
+    obterCotacaoEuro()
+  }, [])
+
   return (
     <div className="bg-zinc-900 text-white hidden sm:block">
       <div className="max-w-7xl mx-auto px-4 xl:px-0 py-3 flex justify-between">
@@ -8,13 +33,13 @@ export function Communication() {
           <p>
             Horário de Portugal: 
             <span className="ml-1 md:ml-2 font-bold">
-              12:30:00
+              {agoraEmLisboa}
             </span>
           </p>
           <p>
             Cotação do Euro: 
             <span className="ml-1 md:ml-2 font-bold">
-              €5,19
+              €{cotacaoEuro.toLocaleString('pt-BR', { maximumFractionDigits: 2 })}
             </span>
           </p>
         </div>
