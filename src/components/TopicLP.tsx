2a6fedc93f505d86b18f015cacf64f2dd948f5c0
J. Vitor F <joaoaovitor.jobs2001@gmail.com>
2023-11-05 11:31:44 -0300

    feat: bug corrigido

--- src/components/TopicLP.tsx
+++ src/components/TopicLP.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react"
+
 interface Props {
   props: {
     topic: number
@@ -7,12 +9,13 @@ interface Props {
 }
 
 export function TopicLP({ props }: Props) {
-  const orientation = props.orientation === "left" ? "right" : "left"
+  const [isLeft] = useState(props.orientation === "left" ? false : true)
+
   return (
     <div className="relative w-8 h-8 rounded-lg bg-main text-center text-second text-2xl font-bold">
       {props.topic}
-      <div className={`absolute top-1/2 -translate-y-1/2 ${orientation}-8 w-24 h-[6px] bg-main`} />
-      <div className={`absolute top-0 -translate-y-0 ${orientation}-36 w-[302px] text-${orientation} text-gray-800 text-base font-normal`}>
+      <div className={`absolute top-1/2 -translate-y-1/2 ${isLeft ? "left-8" : "right-8"} w-24 h-[6px] bg-main`} />
+      <div className={`absolute top-0 -translate-y-0 ${isLeft ? "left-36" : "right-36"} w-[302px] ${isLeft ? "text-left" : "text-right"} text-gray-800 text-base font-normal`}>
         {props.description}
       </div>
     </div>
