e8676ced819d76b3c7b1bde82bddb958138a542f
J. Vitor F <joaoaovitor.jobs2001@gmail.com>
2023-11-29 10:14:39 -0300

    feat: add improvements to footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -15,13 +15,15 @@ interface Props {
 export function Footer({ footer }: Props) {
   return (
     <footer className="relative h-[768px] md:h-[640px] flex justify-center items-center bg-main" id="contact">
-      <iframe
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3111.437361867125!2d-9.202114023589829!3d38.75367155517112!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd1ecd28ecbb3195%3A0x249c855d05306fc!2sR.%20Dr.%20Jo%C3%A3o%20de%20Barros%2015%2C%201500-206%20Lisboa%2C%20Portugal!5e0!3m2!1spt-BR!2sbr!4v1697324594503!5m2!1spt-BR!2sbr"
-        className="w-full h-full border-0"
-        allowFullScreen
-        loading="lazy"
-        referrerPolicy="no-referrer-when-downgrade"
+      <Image
+        alt="Imagem de Portugal"
+        src="/landing-page/example_08.jpg"
+        width={0}
+        height={0}
+        sizes="100vw"
+        className="w-full h-full object-cover"
       />
+      <div className="absolute w-full h-full bg-[#343f2c] opacity-75" />
       <div className="absolute bg-main max-w-7xl w-full mx-auto grid md:grid-cols-2 gap-12 p-4 sm:p-12 rounded-lg">
         <Image
           alt="Logotipo da Mesquita e Dallabrida"
